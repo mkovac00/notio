1502428fc2ef8483f80dd82501a1022b6ca1e4f6
Mario Kovac <mkovac25122512@gmail.com>
2023-03-26 20:50:30 +0200

    Make a note editable when viewing the note

--- src/components/EditNote.tsx
+++ src/components/EditNote.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import "./EditNote.scss";
 
+import { AiOutlineCheckCircle } from "react-icons/ai";
+
 const EditNote = () => {
   const { nid } = useParams();
   let currentNote = {
@@ -19,12 +21,52 @@ const EditNote = () => {
     }
   });
 
+  const saveNoteToLocalStorage = () => {
+    let allNotes = JSON.parse(localStorage.getItem("notes") || "[]");
+    let currentNoteIndex = 0;
+    allNotes.forEach((note: any) => {
+      if (note.id.toString() === nid) {
+        currentNote = note;
+        currentNoteIndex = allNotes.indexOf(note);
+      }
+    });
+
+    const noteTitle = document.getElementById(
+      "edit-note_title-id"
+    ) as HTMLInputElement;
+    const noteContent = document.getElementById(
+      "edit-note_content-id"
+    ) as HTMLInputElement;
+
+    if (noteTitle.value != null && noteTitle.value !== "") {
+      if (noteContent.value != null && noteContent.value !== "") {
+        currentNote.title = noteTitle.value;
+        currentNote.content = noteContent.value;
+      }
+    }
+
+    if (currentNoteIndex !== -1) {
+      allNotes[currentNoteIndex] = currentNote;
+    }
+
+    localStorage.setItem("notes", JSON.stringify(allNotes));
+  };
+
   return (
     <div className="edit-note_container">
       <h1 className="edit-note_container-title">Viewing note</h1>
       <div className="edit-note_section">
-        <textarea className="edit-note_title">{currentNote.title}</textarea>
-        <textarea className="edit-note_content">{currentNote.content}</textarea>
+        <AiOutlineCheckCircle
+          size={25}
+          className="edit-note_btn"
+          onClick={saveNoteToLocalStorage}
+        />
+        <textarea id="edit-note_title-id" className="edit-note_title">
+          {currentNote.title}
+        </textarea>
+        <textarea id="edit-note_content-id" className="edit-note_content">
+          {currentNote.content}
+        </textarea>
         <p className="edit-note_updated">{currentNote.updated}</p>
       </div>
     </div>
